e956b3d0e1c917c60dc03fda5eec59e703a09a78
RakhimovSE <JTzE8RYJeU9x8aw>
2023-09-20 23:14:20 +0400

    Install GraphQL

--- index.ts
+++ index.ts
@@ -1,7 +1,23 @@
 import express, { Express, Request, Response } from "express"
+import { graphqlHTTP } from "express-graphql"
+import { buildSchema } from "graphql"
 import dotenv from "dotenv"
 
 dotenv.config({ override: true })
+
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`)
+
+// The root provides a resolver function for each API endpoint
+var root = {
+  hello: () => {
+    return "Hello world! 123"
+  },
+}
 
 const app: Express = express()
 const port = process.env.PORT
@@ -10,6 +26,18 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server is running")
 })
 
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+)
+
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`)
+  console.log(
+    `Running a GraphQL API server at http://localhost:${port}/graphql`
+  )
 })
