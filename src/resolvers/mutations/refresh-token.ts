123b4c37fd7c8e26d73f8e49ce782e115b93b029
RakhimovSE <JTzE8RYJeU9x8aw>
2023-10-24 00:06:06 +0400

    Create refresh-token.ts resolver

---
+++ src/resolvers/mutations/refresh-token.ts
@@ -0,0 +1,47 @@
+import jwt from "jsonwebtoken"
+
+import { MutationResolvers } from "../../__generated__/resolvers-types"
+import {
+  InvalidRefreshTokenError,
+  UserNotFoundError,
+} from "../../utils/errors/index.js"
+import { AistantApolloContext, RefreshTokenPayload } from "../../types.js"
+import prisma from "../client.js"
+import { hashToken } from "../../utils/jwt.js"
+
+const refreshToken: MutationResolvers<AistantApolloContext>["refreshToken"] =
+  async (_, { refreshToken }) => {
+    const payload = jwt.verify(
+      refreshToken,
+      process.env.JWT_REFRESH_SECRET as string
+    ) as RefreshTokenPayload
+    const savedRefreshToken = await prisma.findRefreshToken(payload.jti)
+
+    if (!savedRefreshToken || savedRefreshToken.revoked) {
+      throw new InvalidRefreshTokenError()
+    }
+
+    const hashedToken = hashToken(refreshToken)
+    if (hashedToken !== savedRefreshToken.hashedToken) {
+      throw new InvalidRefreshTokenError()
+    }
+
+    const user = await prisma.findUser({ id: payload.userId })
+    if (!user) {
+      throw new UserNotFoundError()
+    }
+
+    await prisma.deleteRefreshToken(savedRefreshToken.id)
+    const { accessToken, refreshToken: newRefreshToken } =
+      await prisma.createTokens(user)
+
+    return {
+      code: "200",
+      success: true,
+      message: "Issued new access and refresh tokens successfully",
+      accessToken,
+      refreshToken: newRefreshToken,
+    }
+  }
+
+export default refreshToken
