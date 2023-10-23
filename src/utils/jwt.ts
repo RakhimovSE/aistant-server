270eb3bb5db2e3f60abf22fe31c12e95c15bc7e8
RakhimovSE <JTzE8RYJeU9x8aw>
2023-10-24 00:02:43 +0400

    Move hashToken function to jwt.ts

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,10 +1,11 @@
+import crypto from "crypto"
 import jwt from "jsonwebtoken"
 import { User } from "@prisma/client"
-import { UserPayload } from "../types"
+import { AccessTokenPayload, RefreshTokenPayload } from "../types"
 
 // Usually I keep the token between 5 minutes - 15 minutes
 export function generateAccessToken(user: User) {
-  const data: UserPayload = {
+  const data: AccessTokenPayload = {
     id: user.id,
     email: user.email,
     firstName: user.firstName,
@@ -22,14 +23,15 @@ export function generateAccessToken(user: User) {
 // You can change this value depending on your app logic.
 // I would go for a maximum of 7 days, and make him login again after 7 days of inactivity.
 export function generateRefreshToken(user: User, jti: string) {
-  return jwt.sign(
-    {
-      userId: user.id,
-      jti,
-    },
-    process.env.JWT_REFRESH_SECRET as string,
-    {
-      expiresIn: "8h",
-    }
-  )
+  const data: RefreshTokenPayload = {
+    userId: user.id,
+    jti,
+  }
+  return jwt.sign(data, process.env.JWT_REFRESH_SECRET as string, {
+    expiresIn: "8h",
+  })
+}
+
+export function hashToken(token: string) {
+  return crypto.createHash("sha512").update(token).digest("hex")
 }
